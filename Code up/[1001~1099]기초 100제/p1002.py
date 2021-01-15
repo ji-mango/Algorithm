5bf472c61409806ce5a4c80e137c71c99503d470
ji-mango <susie000301@naver.com>
2021-01-15 18:10:16 +0900

    add basic problem code

---
+++ Code up/[1001~1099]기초 100제/p1002.py
@@ -0,0 +1,7 @@
+'''
+다음 문장을 출력해보자.
+
+Hello World
+(대소문자에 주의한다.)
+'''
+print("Hello World")
---
+++ Code up/[1001~1099]기초 100제/p1003.py
@@ -0,0 +1,9 @@
+'''
+다음과 같이 줄을 바꿔 출력해야 한다.
+
+Hello
+World
+(두 줄에 걸쳐 줄을 바꿔 출력)
+'''
+print("Hello")
+print("World")
